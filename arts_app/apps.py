29b1e09b802d38b6d6af2a7f466a540b41be391c
Raniya <raniyaak4@gmail.com>
2025-12-22 13:05:42 +0530

    22

--- arts_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArtsAppConfig(AppConfig):
-    name = 'arts_app'
---
+++ core_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,'home.html')
+
+def login(request):
+    return render(request,'home.html')
+
+def register(request):
+    return render(request,'home.html')
+
+
+# Create your views here.
